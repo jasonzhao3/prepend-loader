5699d83b59f62b766b2e47bd4327ebc32f39df48
Yang Zhao <yangz@twitter.com>
2015-08-01 14:59:50 -0700

    make prepend-loader cachable

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 "use strict";
- 
+
 var utils = require('loader-utils');
- 
+
 module.exports = function (content) {
-    var opt = utils.parseQuery(this.query);
-    return opt.data ? opt.data + content : content;
+  this.cacheable && this.cacheable();
+  var opt = utils.parseQuery(this.query);
+  return opt.data ? opt.data + content : content;
 };
